410aaef41dcb21127eaf7268461faaa059a51316
Vitor Satyro Vitturi <vitorsv1@al.insper.edu.br>
2025-03-18 19:06:30 -0300

    Adding local ip function to main

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 
 	//Import proxy configuration
 	"github.com/vitorsv1/go-proxy/internal/proxy"
 )
 
+func GetLocalIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddress := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddress.IP
+}
+
 func main() {
 	//Defining the HTTP port
 	port := ":8080"
-	log.Printf("Starting proxy server on %s...", port)
+	log.Printf("Starting proxy server on %s%s...", GetLocalIP(), port)
 
 	http.HandleFunc("/", proxy.HandleRequest)
 
